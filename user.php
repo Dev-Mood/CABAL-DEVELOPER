242a51c59cc478e27d728ebfd5396fbf1f5ea476
Dev-Mood <126387763+Dev-Mood@users.noreply.github.com>
2023-03-16 00:06:35 +0800

    Add files via upload

---
+++ user.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CABAL</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="height:1500px">
+
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: rgba(0,0,0,0.5); border: none; width: 100%; height: 70px;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="background: none; display: flex; align-items: center;"><img src="icon1.png" alt="icon" style="height: 80px; padding-top: 30px;"></a>
+    </div>
+    <ul class="nav navbar-nav pull-right" style="margin-top: 10px;">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Features</a></li>
+      <li><a href="#">Download Client</a></li>
+      <li><a href="#">Login <span class="fa fa-user icon"></span></a></li>
+
+    </ul>
+  </div>
+</nav>
+
+
+   
